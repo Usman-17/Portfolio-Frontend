7108fb5b8b26808c4c0525fc21f9d00a6b9cb728
Muhammad Usman <119290487+Usman-1117@users.noreply.github.com>
2024-07-27 22:31:23 +0500

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
