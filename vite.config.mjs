34dbdc03c191649332f8c65759fea97f8942fadd
Usman-17 <usmandev580@gmail.com>
2024-08-23 22:07:32 +0500

    update server end point

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://portfolio-backend-2nog.onrender.com",
         changeOrigin: true,
       },
     },
