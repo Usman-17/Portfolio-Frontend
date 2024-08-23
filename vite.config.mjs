b5492002fb8dbc381b1bb525d8f1ff63c7f924ca
Usman-17 <usmandev580@gmail.com>
2024-08-23 23:36:32 +0500

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,20 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     // proxy: {
+//     //   "/api": {
+//     //     target: "https://portfolio-backend-2nog.onrender.com",
+//     //     changeOrigin: true,
+//     //   },
+//     // },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,11 +23,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    // proxy: {
-    //   "/api": {
-    //     target: "https://portfolio-backend-2nog.onrender.com",
-    //     changeOrigin: true,
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: "https://portfolio-backend-2nog.onrender.com",
+        changeOrigin: true,
+        secure: false, // Use this if you are working with HTTPS in local development and facing issues.
+        rewrite: (path) => path.replace(/^\/api/, ""), // Optional: if your backend doesn't use the `/api` prefix
+      },
+    },
   },
 });
